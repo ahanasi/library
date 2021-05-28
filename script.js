6a852331f6ccc0e83dc422ff5a0f89ba64f167a2
XXX Ahana <ahanasi@seas.upenn.edu>
2021-05-27 17:07:05 -0700

    Create form submission mechanism for new books

--- script.js
+++ script.js
@@ -1,29 +1,68 @@
-const BOOKSHELF = document.querySelector(".bookshelf");
-const TEMPLATE = document.getElementById("card");
+const bookshelf = document.querySelector(".bookshelf");
+const template = document.getElementById("card");
+const modal = document.querySelector(".modal");
+const modalBtns = document.querySelectorAll(".modal-toggle");
+const submitBtn = document.querySelector(".submit-button");
+const entryForm = document.getElementById("entryForm");
+const checkbox = document.getElementById("hasRead");
+
+modalBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    modal.classList.toggle("hidden");
+    modal.classList.toggle("flex");
+  });
+});
 
 let myLibrary = [
-  { title: "Test", author: "Me", pages: 500, read: true },
-  { title: "Harry Potter", author: "J.K. Rowling", pages: 500, read: true },
-  { title: "Twilight", author: "Stephanie Meyer", pages: 200, read: false },
-  { title: "Rhythm of War", author: "Brandon Sanderson", pages: 900, read: true },
-  { title: "Name of the Wind", author: "Patrick Rothfuss", pages: 450, read: true },
+  { title: "Test", author: "Me", pages: 500, hasRead: true },
+  { title: "Harry Potter", author: "J.K. Rowling", pages: 500, hasRead: true },
+  { title: "Twilight", author: "Stephanie Meyer", pages: 200, hasRead: false },
+  { title: "Rhythm of War", author: "Brandon Sanderson", pages: 900, hasRead: true },
+  { title: "Name of the Wind", author: "Patrick Rothfuss", pages: 450, hasRead: true },
 ];
 
-function Book(title, author, pages, read) {
+entryForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let formData = new FormData(entryForm);
+  let book = new Book();
+
+  for (var pair of formData.entries()) {
+    book[pair[0]] = pair[1];
+  }
+
+  setCheckboxVal(book);
+  addBookToLibrary(book);
+  entryForm.reset();
+  clearLibrary();
+  showLibrary();
+});
+
+function setCheckboxVal(book) {
+  return checkbox.checked ? (book.hasRead = true) : (book.hasRead = false);
+}
+
+function Book(title, author, pages, hasRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.hasRead = hasRead;
 }
 
 function addBookToLibrary(book) {
   return myLibrary.push(book);
 }
 
+function clearLibrary() {
+  while (bookshelf.firstChild) {
+    bookshelf.removeChild(bookshelf.firstChild);
+  }
+}
+
 function showLibrary() {
   myLibrary.forEach((book) => {
     if ("content" in document.createElement("template")) {
-      let clone = TEMPLATE.content.cloneNode(true);
+      let clone = template.content.cloneNode(true);
       let title = clone.querySelector("h2");
       title.textContent = book.title;
 
@@ -31,8 +70,7 @@ function showLibrary() {
       p[0].textContent = book.author;
       p[1].textContent = `${book.pages} pages`;
 
-      BOOKSHELF.appendChild(clone);
-
+      bookshelf.appendChild(clone);
     }
   });
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: [],
@@ -9,15 +9,22 @@ module.exports = {
         "smoke-darkest": "var(--smoke-darkest)",
         "smoke-darker": "var(--smoke-darker)",
         "smoke-dark": "var(--smoke-dark)",
-        "smoke": "var(--smoke)",
+        smoke: "var(--smoke)",
         "smoke-light": "var(--smoke-light)",
         "smoke-lighter": "var(--smoke-lighter)",
         "smoke-lightest": "var(--smoke-lightest)",
       },
+    },
+    minWidth: {
+      0: "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
