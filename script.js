5767cf2985fcaee0577f2ebf3481d5c81031fbec
XXX Ahana <ahanasi@seas.upenn.edu>
2021-06-02 18:26:41 -0700

    Moved function calls in index.html to script.js

--- script.js
+++ script.js
@@ -2,66 +2,38 @@ const bookshelf = document.querySelector(".bookshelf");
 const template = document.getElementById("card");
 const modal = document.querySelector(".modal");
 const modalBtns = document.querySelectorAll(".modal-toggle");
-const submitBtn = document.querySelector(".submit-button");
 const entryForm = document.getElementById("entryForm");
 const checkbox = document.getElementById("hasRead");
 
-let myLibrary = [
-  new Book("Test","Me",648,true),
-  new Book("Harry Potter", "J.K. Rowling",500,true),
-  new Book("Twilight","Stephanie Meyer",450,false),
-  new Book("Rhythm of War","Brandon Sanderson",1000,true),
-  new Book("Name of the Wind","Patrick Rothfuss", 1500, true)
-];
-
-modalBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    modal.classList.toggle("hidden");
-    modal.classList.toggle("flex");
-  });
-});
-
-entryForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  let formData = new FormData(entryForm);
-  let book = new Book();
-
-  for (var pair of formData.entries()) {
-    book[pair[0]] = pair[1];
+class Book {
+  constructor(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
   }
 
-  setCheckboxVal(book);
-  addBookToLibrary(book);
-  entryForm.reset();
-  clearLibrary();
-  showLibrary();
-});
-
-function Book(title, author, pages, hasRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.hasRead = hasRead;
+  setReadStatus(status) {
+    this.hasRead = status;
+  }
 }
 
-Book.prototype.setReadStatus = function (status) {
-  this.hasRead = status;
-};
-
-function deleteBook(btn) {
-  myLibrary.splice(btn.dataset.index, 1);
-  clearLibrary();
-  showLibrary();
-}
+const myLibrary = [
+  new Book("Test", "Me", 648, true),
+  new Book("Harry Potter", "J.K. Rowling", 500, true),
+  new Book("Twilight", "Stephanie Meyer", 450, false),
+  new Book("Rhythm of War", "Brandon Sanderson", 1000, true),
+  new Book("Name of the Wind", "Patrick Rothfuss", 1500, true),
+];
 
 function toggleReadBtn(btn) {
-  if (btn.textContent == "Read ✅") {
-    myLibrary[btn.dataset.index].setReadStatus(true);
-    btn.textContent = "Not Read ❌";
-  } else {
+  const readBtn = btn;
+  if (readBtn.textContent === "Read ✅") {
     myLibrary[btn.dataset.index].setReadStatus(false);
-    btn.textContent = "Read ✅";
+    readBtn.textContent = "Not Read ❌";
+  } else {
+    myLibrary[readBtn.dataset.index].setReadStatus(true);
+    readBtn.textContent = "Read ✅";
   }
 
   btn.classList.toggle("text-green-700");
@@ -85,25 +57,25 @@ function clearLibrary() {
 function showLibrary() {
   myLibrary.forEach((book, i) => {
     if ("content" in document.createElement("template")) {
-      let clone = template.content.cloneNode(true);
+      const clone = template.content.cloneNode(true);
 
-      let delBtn = clone.querySelector("button.delete-book");
-      let hasReadBtn = clone.querySelector("button.hasRead");
+      const delBtn = clone.querySelector("button.delete-book");
+      const hasReadBtn = clone.querySelector("button.hasRead");
       delBtn.setAttribute("data-index", i);
       hasReadBtn.setAttribute("data-index", i);
 
       if (book.hasRead) {
-        hasReadBtn.textContent = "Not Read ❌";
+        hasReadBtn.textContent = "Read ✅";
         hasReadBtn.classList.toggle("text-green-700");
         hasReadBtn.classList.toggle("text-red-700");
       } else {
-        hasReadBtn.textContent = "Read ✅";
+        hasReadBtn.textContent = "Not Read ❌";
       }
 
-      let title = clone.querySelector("h2");
+      const title = clone.querySelector("h2");
       title.textContent = book.title;
 
-      let p = clone.querySelectorAll("p");
+      const p = clone.querySelectorAll("p");
       p[0].textContent = book.author;
       p[1].textContent = `${book.pages} pages`;
 
@@ -111,5 +83,51 @@ function showLibrary() {
     }
   });
 }
+
+function deleteBook(btn) {
+  myLibrary.splice(btn.dataset.index, 1);
+  clearLibrary();
+  showLibrary();
+}
+
+modalBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+    modal.classList.toggle("flex");
+  });
+});
+
+document.body.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-book")) {
+    deleteBook(event.target);
+  } else if (
+    event.target.classList.contains("hasRead") &&
+    event.target.type === "submit"
+  ) {
+    toggleReadBtn(event.target);
+  }
+});
+
+entryForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(entryForm);
+  const book = new Book();
+
+  // for (const pair of formData.entries()) {
+  //   book[pair[0]] = pair[1];
+  // }
+
+  [...formData.entries()].forEach((pair) => {
+    const [key, value] = pair;
+    book[key] = value;
+  });
+
+  setCheckboxVal(book);
+  addBookToLibrary(book);
+  entryForm.reset();
+  clearLibrary();
+  showLibrary();
+});
 
 showLibrary();
